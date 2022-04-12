ca4a04267a77a38f682c6709bccbf9e2187af4c8
wxx <614610440@qq.com>
2022-04-12 21:44:21 +0800

    add gmp

---
+++ GMP/README.md
@@ -0,0 +1,5 @@
+## trace测试
+```shell
+go run main.go
+go tool trace trace.out
+```
---
+++ GMP/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"runtime/trace"
+)
+
+func main() {
+	f, err := os.Create("trace.out")
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer f.Close()
+
+	err = trace.Start(f)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Hello World!")
+
+	trace.Stop()
+}
